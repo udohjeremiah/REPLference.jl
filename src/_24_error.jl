1d22b7067543411c8cb57486b7d160cb543d25d7
Udoh Jeremiah <udohjeremiah@icloud.com>
2023-05-01 23:38:01 +0100

    add functions for error.jl

---
+++ src/_24_error.jl
@@ -0,0 +1,33 @@
+"""
+"""
+function errors(; extmod=false)
+    constants = [
+        "Constants" => [
+            "stderr"
+        ]
+    ]
+    macros = [
+        "Macros" => [
+            "@debug", "@error", "@info", "@warn"
+        ]
+    ]
+    methods = [
+        "Methods" => [
+            "capture_exceptionˣ", "catch_backtrace", "current_exceptions", "error",
+            "errormonitor", "identity", "redirect_stderr", "redirect_stdio", "rethrow",
+            "retry", "showerror", "stacktrace", "systemerror", "throw", "windowserrorˣ",
+        ]
+    ]
+    types = [
+        "Types" => [
+            "ArgumentError", "AssertionError", "BoundsError", "CompositeException",
+            "DimensionMismatch", "DivideError", "DomainError", "EOFError", "ErrorException",
+            "InexactError", "InitError", "InterruptException", "KeyError", "LoadError",
+            "MethodError", "MissingException", "OutOfMemoryError", "OverflowError",
+            "ProcessFailedException", "ReadOnlyMemoryError", "StackOverflowError",
+            "StringIndexError", "SystemError", "TaskFailedException", "TypeError",
+            "UndefKeywordError", "UndefRefError", "UndefVarError", "WrappedExceptionˣ"
+        ]
+    ]
+    _print_names(constants, macros, methods, types)
+end
